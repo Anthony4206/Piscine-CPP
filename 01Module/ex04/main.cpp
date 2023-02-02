00866dada82cb06fcf765aa8407a71be6c8ab389
Anthony Levasseur <alevasse@student.42nice.fr>
2023-02-02 13:48:42 +0100

    01Module

--- 01Module/ex04/main.cpp
+++ 01Module/ex04/main.cpp
@@ -3,27 +3,36 @@
 #include <string>
 
 int main(int argc, char **argv) {
-    std::ifstream   input_file(argv[1]);
-    std::ofstream   output_file;
-    std::string     line;
-
-    std::cout << "lol" << std::endl;
 
     if (argc != 4) {
         std::cerr << "./replace <file> <s1> <s2>" << std::endl;
         return (-1);
     }
+
+    std::ifstream   input_file(argv[1]);
     if (!input_file.is_open()) {
         std::cerr << "failed" << std::endl;
         return (-1);
     }
+
+	std::string		outfile(argv[1]);
+    std::ofstream	output_file(outfile + ".replace");
     if (!output_file.is_open()) {
         std::cerr << "failed" << std::endl;
         input_file.close();
         return (-1);
     }
+
+	std::string	s1(argv[2]);
+	std::string	s2(argv[3]);
+    std::string	line;
+	std::size_t	found;
     while (getline(input_file, line)) {
-        std::cout << line << std::endl;
-        line.find(argv[2]);
+		found = line.find(s1);
+        if (found != std::string::npos) {
+			output_file << line.substr(0, found) << s2 << std::endl;
+
+		} else
+			output_file << line << std::endl;
     }
 }
